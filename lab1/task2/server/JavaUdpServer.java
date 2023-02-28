c3dcf67cb219f3805a952ab65e6e383db4ca9514
Tomasz Ostafin <ostafintomasz1@gmail.com>
2023-02-28 23:28:18 +0100

    lab1: add task2

---
+++ lab1/task2/server/JavaUdpServer.java
@@ -0,0 +1,26 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class JavaUdpServer {
+    public static void main(String[] args) {
+        System.out.println("JAVA UDP SERVER");
+
+        int portNumber = 9008;
+        try (DatagramSocket socket = new DatagramSocket(portNumber)) {
+            byte[] receiveBuffer = new byte[1024];
+
+            while (true) {
+                Arrays.fill(receiveBuffer, (byte) 0);
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                socket.receive(receivePacket);
+                String msg = new String(receivePacket.getData());
+                System.out.println("received msg: " + msg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
